700b88f060d90ab8d7a6cb2bb70729526d0ddcc3
SandyPati28 <patilsandesh976@gmail.com>
2022-09-19 18:27:44 +0530

    By Team

--- com.advanceJava/src/com/advanceJava/Emp.java
+++ com.advanceJava/src/com/advanceJava/Emp.java
@@ -2,6 +2,18 @@ package com.advanceJava;
 
 public class Emp {
    private String city;
+   
+   private String pune;
+   
+   
+
+public String getPune() {
+	return pune;
+}
+
+public void setPune(String pune) {
+	this.pune = pune;
+}
 
 public String getCity() {
 	return city;
